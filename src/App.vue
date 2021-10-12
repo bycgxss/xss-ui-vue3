910169e1cac265b66c68b734130026135c640492
bycgxss <bycgxss@gmail.com>
2021-10-12 15:38:16 +0800

    Build HTML structure

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,4 @@
 <template>
-  <div>
-    导航栏 |
-    <router-link to="/">Xss</router-link> |
-    <router-link to="/xxx">Xss2</router-link>
-  </div>
   <router-view />
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import { createWebHashHistory, createRouter } from 'vue-router'
-import Xss from './components/Xss.vue'
-import Xss2 from './components/Xss2.vue'
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
 
 const history = createWebHashHistory()
 const router = createRouter({
   history: history,
   routes: [
-    { path: '/', component: Xss },
-    { path: '/xxx', component: Xss2 },
+    { path: '/', component: Home },
+    { path: '/doc', component: Doc },
   ]
 })
 
